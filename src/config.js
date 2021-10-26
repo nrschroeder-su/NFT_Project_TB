41814b8e8a4801a38292555db62c6bf59c316468
Noah <nrschroe@syr.edu>
2021-10-26 11:27:25 -0400

    NFT Commit

---
+++ src/config.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "src/blendMode.js"));
+const description =
+  "This is the description of your NFT project, remember to replace this";
+const baseUri = "ipfs://NewUriToReplace";
+
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 250,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Base Color" },
+      { name: "Bear Eyes" },
+      { name: "Bear Hat" },
+      { name: "Bear Nose" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 1000,
+  height: 1000,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.width / format.height,
+  imageName: "preview.png",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+};
